fb9553bb058bea910e685d18a7d19cd7d330583f
myself-nate <114106863+myself-nate@users.noreply.github.com>
2026-05-14 16:01:35 +0300

    test

--- sh-website/src/app/(nondashboard)/search/page.tsx
+++ sh-website/src/app/(nondashboard)/search/page.tsx
@@ -1,9 +1,76 @@
+// 'use client';
+
+// import { NAVBAR_HEIGHT } from '@/lib/constants';
+// import { useAppDispatch, useAppSelector } from '@/state/redux';
+// import { useSearchParams } from 'next/navigation';
+// import React, { useEffect } from 'react';
+// import FiltersBar from './FiltersBar';
+// import FiltersFull from './FiltersFull';
+// import { cleanParams } from '@/lib/utils';
+// import { setFilters } from '@/state';
+// import Map from './Map';
+// import Listings from './Listings';
+
+// const SearchPage = () => {
+// 	const searchParams = useSearchParams();
+// 	const dispatch = useAppDispatch();
+// 	const isFiltersFullOpen = useAppSelector(
+// 		(state) => state.global.isFiltersFullOpen,
+// 	);
+
+// 	useEffect(() => {
+// 		const initialFilters = Array.from(searchParams.entries()).reduce(
+// 			(acc: any, [key, value]) => {
+// 				if (key === 'coordinates') {
+// 					acc[key] = value.split(',').map(Number);
+// 				} else {
+// 					acc[key] = value === 'any' ? null : value;
+// 				}
+
+// 				return acc;
+// 			},
+// 			{},
+// 		);
+
+// 		const cleanedFilters = cleanParams(initialFilters);
+// 		dispatch(setFilters(cleanedFilters));
+// 	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+// 	return (
+// 		<div
+// 			className="w-full mx-auto px-5 flex flex-col"
+// 			style={{
+// 				height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
+// 			}}
+// 		>
+// 			<FiltersBar />
+// 			<div className="flex justify-between flex-1 overflow-hidden gap-3 mb-5">
+// 				<div
+// 					className={`h-full overflow-auto transition-all duration-300 ease-in-out ${
+// 						isFiltersFullOpen
+// 							? 'w-3/12 opacity-100 visible'
+// 							: 'w-0 opacity-0 invisible'
+// 					}`}
+// 				>
+// 					<FiltersFull />
+// 				</div>
+// 				<Map />
+// 				<div className="basis-4/12 overflow-y-auto">
+// 					<Listings />
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default SearchPage;
+
 'use client';
 
 import { NAVBAR_HEIGHT } from '@/lib/constants';
 import { useAppDispatch, useAppSelector } from '@/state/redux';
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import FiltersBar from './FiltersBar';
 import FiltersFull from './FiltersFull';
 import { cleanParams } from '@/lib/utils';
@@ -11,7 +78,7 @@ import { setFilters } from '@/state';
 import Map from './Map';
 import Listings from './Listings';
 
-const SearchPage = () => {
+function SearchPageContent() {
 	const searchParams = useSearchParams();
 	const dispatch = useAppDispatch();
 	const isFiltersFullOpen = useAppSelector(
@@ -26,7 +93,6 @@ const SearchPage = () => {
 				} else {
 					acc[key] = value === 'any' ? null : value;
 				}
-
 				return acc;
 			},
 			{},
@@ -34,7 +100,7 @@ const SearchPage = () => {
 
 		const cleanedFilters = cleanParams(initialFilters);
 		dispatch(setFilters(cleanedFilters));
-	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+	}, [searchParams, dispatch]);
 
 	return (
 		<div
@@ -61,6 +127,18 @@ const SearchPage = () => {
 			</div>
 		</div>
 	);
-};
+}
 
-export default SearchPage;
+export default function SearchPage() {
+	return (
+		<Suspense
+			fallback={
+				<div className="flex items-center justify-center h-screen">
+					Loading search...
+				</div>
+			}
+		>
+			<SearchPageContent />
+		</Suspense>
+	);
+}
--- sh-website/src/types/index.d.ts
+++ sh-website/src/types/index.d.ts
@@ -1,140 +1,143 @@
-import { LucideIcon } from "lucide-react";
-import { AuthUser } from "aws-amplify/auth";
-import { Manager, Tenant, Property, Application } from "./prismaTypes";
-import { MotionProps as OriginalMotionProps } from "framer-motion";
+import { LucideIcon } from 'lucide-react';
+import { AuthUser } from 'aws-amplify/auth';
+import { Manager, Tenant, Property, Application } from './prismaTypes';
+import { MotionProps as OriginalMotionProps } from 'framer-motion';
 
-declare module "framer-motion" {
-  interface MotionProps extends OriginalMotionProps {
-    className?: string;
-  }
+declare module 'framer-motion' {
+	interface MotionProps extends OriginalMotionProps {
+		className?: string;
+	}
 }
 
 declare global {
-  enum AmenityEnum {
-    WasherDryer = "WasherDryer",
-    AirConditioning = "AirConditioning",
-    Dishwasher = "Dishwasher",
-    HighSpeedInternet = "HighSpeedInternet",
-    HardwoodFloors = "HardwoodFloors",
-    WalkInClosets = "WalkInClosets",
-    Microwave = "Microwave",
-    Refrigerator = "Refrigerator",
-    Pool = "Pool",
-    Gym = "Gym",
-    Parking = "Parking",
-    PetsAllowed = "PetsAllowed",
-    WiFi = "WiFi",
-  }
+	enum AmenityEnum {
+		CabineDeProba = 'CabineDeProba',
+		Toaleta = 'Toaleta',
+		ApaPotabila = 'ApaPotabila',
+		// WasherDryer = "WasherDryer",
+		// AirConditioning = "AirConditioning",
+		// Dishwasher = "Dishwasher",
+		// HighSpeedInternet = "HighSpeedInternet",
+		// HardwoodFloors = "HardwoodFloors",
+		// WalkInClosets = "WalkInClosets",
+		// Microwave = "Microwave",
+		// Refrigerator = "Refrigerator",
+		// Pool = "Pool",
+		// Gym = "Gym",
+		// Parking = "Parking",
+		// PetsAllowed = "PetsAllowed",
+		WiFi = 'WiFi',
+	}
 
-  enum HighlightEnum {
-    HighSpeedInternetAccess = "HighSpeedInternetAccess",
-    WasherDryer = "WasherDryer",
-    AirConditioning = "AirConditioning",
-    Heating = "Heating",
-    SmokeFree = "SmokeFree",
-    CableReady = "CableReady",
-    SatelliteTV = "SatelliteTV",
-    DoubleVanities = "DoubleVanities",
-    TubShower = "TubShower",
-    Intercom = "Intercom",
-    SprinklerSystem = "SprinklerSystem",
-    RecentlyRenovated = "RecentlyRenovated",
-    CloseToTransit = "CloseToTransit",
-    GreatView = "GreatView",
-    QuietNeighborhood = "QuietNeighborhood",
-  }
+	enum HighlightEnum {
+		HighSpeedInternetAccess = 'HighSpeedInternetAccess',
+		WasherDryer = 'WasherDryer',
+		AirConditioning = 'AirConditioning',
+		Heating = 'Heating',
+		SmokeFree = 'SmokeFree',
+		CableReady = 'CableReady',
+		SatelliteTV = 'SatelliteTV',
+		DoubleVanities = 'DoubleVanities',
+		TubShower = 'TubShower',
+		Intercom = 'Intercom',
+		SprinklerSystem = 'SprinklerSystem',
+		RecentlyRenovated = 'RecentlyRenovated',
+		CloseToTransit = 'CloseToTransit',
+		GreatView = 'GreatView',
+		QuietNeighborhood = 'QuietNeighborhood',
+	}
 
-  enum PropertyTypeEnum {
-    Rooms = "Rooms",
-    Tinyhouse = "Tinyhouse",
-    Apartment = "Apartment",
-    Villa = "Villa",
-    Townhouse = "Townhouse",
-    Cottage = "Cottage",
-  }
+	enum PropertyTypeEnum {
+		Rooms = 'Rooms',
+		Tinyhouse = 'Tinyhouse',
+		Apartment = 'Apartment',
+		Villa = 'Villa',
+		Townhouse = 'Townhouse',
+		Cottage = 'Cottage',
+	}
 
-  interface SidebarLinkProps {
-    href: string;
-    icon: LucideIcon;
-    label: string;
-  }
+	interface SidebarLinkProps {
+		href: string;
+		icon: LucideIcon;
+		label: string;
+	}
 
-  interface PropertyOverviewProps {
-    propertyId: number;
-  }
+	interface PropertyOverviewProps {
+		propertyId: number;
+	}
 
-  interface ApplicationModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    propertyId: number;
-  }
+	interface ApplicationModalProps {
+		isOpen: boolean;
+		onClose: () => void;
+		propertyId: number;
+	}
 
-  interface ContactWidgetProps {
-    onOpenModal: () => void;
-  }
+	interface ContactWidgetProps {
+		onOpenModal: () => void;
+	}
 
-  interface ImagePreviewsProps {
-    images: string[];
-  }
+	interface ImagePreviewsProps {
+		images: string[];
+	}
 
-  interface PropertyDetailsProps {
-    propertyId: number;
-  }
+	interface PropertyDetailsProps {
+		propertyId: number;
+	}
 
-  interface PropertyOverviewProps {
-    propertyId: number;
-  }
+	interface PropertyOverviewProps {
+		propertyId: number;
+	}
 
-  interface PropertyLocationProps {
-    propertyId: number;
-  }
+	interface PropertyLocationProps {
+		propertyId: number;
+	}
 
-  interface ApplicationCardProps {
-    application: Application;
-    userType: "manager" | "renter";
-    children: React.ReactNode;
-  }
+	interface ApplicationCardProps {
+		application: Application;
+		userType: 'manager' | 'renter';
+		children: React.ReactNode;
+	}
 
-  interface CardProps {
-    property: Property;
-    isFavorite: boolean;
-    onFavoriteToggle: () => void;
-    showFavoriteButton?: boolean;
-    propertyLink?: string;
-  }
+	interface CardProps {
+		property: Property;
+		// isFavorite: boolean;
+		// onFavoriteToggle: () => void;
+		// showFavoriteButton?: boolean;
+		propertyLink?: string;
+	}
 
-  interface CardCompactProps {
-    property: Property;
-    isFavorite: boolean;
-    onFavoriteToggle: () => void;
-    showFavoriteButton?: boolean;
-    propertyLink?: string;
-  }
+	interface CardCompactProps {
+		property: Property;
+		// isFavorite: boolean;
+		// onFavoriteToggle: () => void;
+		// showFavoriteButton?: boolean;
+		propertyLink?: string;
+	}
 
-  interface HeaderProps {
-    title: string;
-    subtitle: string;
-  }
+	interface HeaderProps {
+		title: string;
+		subtitle: string;
+	}
 
-  interface NavbarProps {
-    isDashboard: boolean;
-  }
+	interface NavbarProps {
+		isDashboard: boolean;
+	}
 
-  interface AppSidebarProps {
-    userType: "manager" | "tenant";
-  }
+	interface AppSidebarProps {
+		userType: 'manager' | 'tenant';
+	}
 
-  interface SettingsFormProps {
-    initialData: SettingsFormData;
-    onSubmit: (data: SettingsFormData) => Promise<void>;
-    userType: "manager" | "tenant";
-  }
+	interface SettingsFormProps {
+		initialData: SettingsFormData;
+		onSubmit: (data: SettingsFormData) => Promise<void>;
+		userType: 'manager' | 'tenant';
+	}
 
-  interface User {
-    cognitoInfo: AuthUser;
-    userInfo: Tenant | Manager;
-    userRole: JsonObject | JsonPrimitive | JsonArray;
-  }
+	interface User {
+		cognitoInfo: AuthUser;
+		userInfo: Tenant | Manager;
+		userRole: JsonObject | JsonPrimitive | JsonArray;
+	}
 }
 
 export {};

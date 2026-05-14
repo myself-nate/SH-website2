16d413611c240f336d8d711415d448b9358838f2
myself-nate <114106863+myself-nate@users.noreply.github.com>
2026-05-14 14:48:20 +0300

    tryu again

--- sh-website/next.config.ts
+++ sh-website/next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	turbopack: false,
-
-	webpack: (config, { isServer, dev }) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			lightningcss: false,
-		};
-		return config;
-	},
-
 	images: {
 		remotePatterns: [
 			{
